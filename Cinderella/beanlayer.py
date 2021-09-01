500de98aa997d8854d987c3ddd9178dd78f30c0d
Daniel Tsai <idaniel.twc@gmail.com>
2021-09-01 20:50:22 +0800

    prototype interface with beancount

--- Cinderella/beanlayer.py
+++ Cinderella/beanlayer.py
@@ -1,6 +1,9 @@
 from configs import Configs
 from datatypes import Directives
 
+import beancount.parser.parser as beancount_parser
+from beancount.core.data import Transaction
+
 
 class BeanCountAPI:
 
@@ -20,3 +23,12 @@ class BeanCountAPI:
         with open(path, "a") as f:
             for directive in directives:
                 f.write(directive.to_bean())
+
+    def read_bean(self, path: str) -> list[Transaction]:
+        return beancount_parser.parse_file(path)[0]
+
+if __name__ == "__main__":
+    bc = BeanCountAPI()
+    ledger = bc.read_bean("../beans/custom.bean")
+    print((ledger))
+
