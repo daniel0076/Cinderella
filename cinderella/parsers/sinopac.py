b4afaad27b81137043fd837d57c00149fa2de962
Daniel Tsai <idaniel.twc@gmail.com>
2021-11-15 22:01:30 +0800

    Fix sinopac foreign exchange handling

--- cinderella/parsers/sinopac.py
+++ cinderella/parsers/sinopac.py
@@ -51,7 +51,7 @@ class Sinopac(StatementParser):
         transactions = Transactions(category, self.identifier)
 
         for _, record in records.iterrows():
-            date = datetime.strptime(record[1].lstrip(), "%Y/%m/%d")
+            date = datetime.strptime(record[0].lstrip().split(" ")[0], "%Y/%m/%d")
             title = record[2]
             if record[3] == " ":
                 price = Decimal(record[4])
@@ -69,20 +69,22 @@ class Sinopac(StatementParser):
             rate = Decimal(str(record[6])) if not np.isnan(record[6]) else None
             if rate:
                 foreign_currency = re.search(r"\(([A-Z]*)\)", str(record[7])).group(1)  # xxxxxx(USD)
-                foreign_price = self.beancount_api.make_amount(rate, foreign_currency)
+                foreign_price = self.beancount_api.make_amount(rate, currency)
                 local_amount = self.beancount_api.make_amount(price, currency)
                 foreign_amount = self.beancount_api.make_amount(Decimal(-price/rate), foreign_currency)
 
                 local_posting = self.beancount_api.make_posting(account, amount=local_amount)
                 foreign_posting = self.beancount_api.make_posting(account, foreign_amount, price=foreign_price)
-                loss_posting = self.beancount_api.make_posting(self.configs.default_accounts["exchange_pnl"], None)
+                pnl_posting = self.beancount_api.make_posting(self.configs.default_accounts["exchange_pnl"], None)
 
-                transaction = self.beancount_api.make_transaction(date, title, [local_posting, foreign_posting, loss_posting])
+                transaction = self.beancount_api.make_transaction(date, title, [local_posting, foreign_posting, pnl_posting])
 
             else:
                 transaction = self.beancount_api.make_simple_transaction(
                     date, title, account, price, currency
                 )
+
+            self.beancount_api.add_transaction_comment(transaction, str(record[7]))
 
             transactions.append(transaction)
 
